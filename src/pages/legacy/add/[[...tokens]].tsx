5fc4a7acdcae47c9cf4f2a1f77c087d423e642f6
ninja-1337 <evangelou.e.95@gmail.com>
2022-03-24 14:26:12 +0000

    sdk

--- src/pages/legacy/add/[[...tokens]].tsx
+++ src/pages/legacy/add/[[...tokens]].tsx
@@ -2,7 +2,6 @@ import { BigNumber } from '@ethersproject/bignumber'
 import { TransactionResponse } from '@ethersproject/providers'
 import { t } from '@lingui/macro'
 import { useLingui } from '@lingui/react'
-import { Currency, CurrencyAmount, currencyEquals, WNATIVE } from '@sushiswap/core-sdk'
 import Alert from 'app/components/Alert'
 import Button from 'app/components/Button'
 import { AutoColumn } from 'app/components/Column'
@@ -43,6 +42,7 @@ import { useRouter } from 'next/router'
 import React, { useCallback, useState } from 'react'
 import { Plus } from 'react-feather'
 import ReactGA from 'react-ga'
+import { Currency, CurrencyAmount, currencyEquals, WNATIVE } from 'souvlaswap-core-sdk'
 
 export default function Add() {
   const { i18n } = useLingui()
