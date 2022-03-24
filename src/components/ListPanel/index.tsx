ba1f80cf0fa5aa08e3ede5c9e22114e7eec93129
ninja-1337 <evangelou.e.95@gmail.com>
2022-03-24 14:48:36 +0000

    sdk

--- src/components/ListPanel/index.tsx
+++ src/components/ListPanel/index.tsx
@@ -1,7 +1,8 @@
-import { Currency, CurrencyAmount, ZERO } from '@sushiswap/core-sdk'
+import { Currency, CurrencyAmount } from '@sushiswap/core-sdk'
 import { classNames } from 'app/functions'
 import { useUSDCValue } from 'app/hooks/useUSDCPrice'
 import React, { FC, ReactNode } from 'react'
+import { ZERO } from 'souvlaswap-core-sdk'
 
 import Chip from '../Chip'
 import { CurrencyLogo } from '../CurrencyLogo'
