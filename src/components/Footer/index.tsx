060cb92b7645fc4ad61fa57181013440f08a8551
ninja-1337 <evangelou.e.95@gmail.com>
2022-03-26 10:12:44 +0000

    theme

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -17,12 +17,12 @@ const Footer = () => {
 
   return (
     <div className="z-10 w-full py-20 mt-20">
-      <Container maxWidth="7xl" className="mx-auto px-6">
-        <div className="grid grid-cols-2 gap-10 pt-8 md:grid-cols-3 lg:grid-cols-6 xs:px-6 border-t border-dark-900">
+      <Container maxWidth="7xl" className="px-6 mx-auto">
+        <div className="grid grid-cols-2 gap-10 pt-8 border-t md:grid-cols-3 lg:grid-cols-6 xs:px-6 border-dark-900">
           <div className="flex flex-col gap-3">
             <div className="flex items-center justify-start gap-2">
               <div className="">
-                <Image src="https://app.sushi.com/images/logo.svg" alt="Sushi logo" width="28px" height="28px" />
+                <Image src="/public/images/logo.svg" alt="SouvlaSwap logo" width="28px" height="28px" />
               </div>
               <Typography variant="h2" weight={700} className="tracking-[0.02em] scale-y-90 hover:text-high-emphesis">
                 Sushi
